35469e19bba90b4f9dc8fae51dfccef03f39c7ff
Xi <xqiu1@paypal.com>
2026-04-07 13:38:34 -0700

    Update train

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -1,12 +1,14 @@
 import torch as t
 import torch.nn as nn
 from jaxtyping import Float
+from dataclasses import dataclass, field
+from torch.utils.data import DataLoader
 
 
 class LogisticRegression(nn.Module):
     def __init__(self, n_features: int, n_classes: int):
         super().__init__()
-        self.linear = nn.Linear(n_features, n_classes),
+        self.linear = nn.Linear(n_features, n_classes)
 
     def forward(
             self, 
@@ -17,18 +19,46 @@ class LogisticRegression(nn.Module):
         out = self.softmax(logits)
         return out
 
-    def softmax(self, x):
+    def softmax(self, x: Float[Tensor, "batch n_features"]) -> Float[Tensor, "batch n_features"]:
         prob_sum = x.exp().sum(dim=-1)
         probs = x.exp() / prob_sum
         return probs
 
-class LogisticRegressionTrainer:
-    def __init__(self):
-        pass
+@dataclass
+class TrainConfig:
+    lr: float = 1e-4
+    epochs: int = 3
+    train_loader: DataLoader
+    test_loader: DataLoader
 
-    def train(self):
-        pass
+
+class LogisticRegressionTrainer:
+    def __init__(self, model: LogisticRegression, config: TrainConfig):
+        self.model = model
+        self.epochs = config.epochs
+        self.optimizer = t.optim.AdamW(config.lr)
+        self.train_loader = config.train_loader
+        self.test_loader = config.test_loader
+
+    def train(self) -> Float[Tensor, ""]:
+        for epoch in range(self.epochs):
+            for X, y in self.train_loader:
+                self.optimizer.zero_grad()
+                y_pred = self.model(X)
+                loss = self.cross_entropy(y, y_pred)
+                loss.backward()
+                self.optimizer.step()
+        return loss
 
     @t.inference_mode()
     def evaluate(self):
-        pass
+        for X, y in self.test_loader:
+
+
+    def cross_entropy(
+            self, 
+            y: Float[Tensor, "batch n_classes"], 
+            y_pred: Float[Tensor, "batch n_classes"]
+        ) -> Float[Tensor, ""]:
+        ce = -(y * y_pred.log()).sum(dim=-1).mean()
+        return ce
